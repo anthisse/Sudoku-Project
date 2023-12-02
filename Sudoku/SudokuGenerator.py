bc9dccdb782e4c41ebcf8043f05de288eb3143c0
s11374832 <adrielspoon@gmail.com>
2023-12-02 00:15:52 -0500

    WOOO

---
+++ Sudoku/SudokuGenerator.py
@@ -0,0 +1,11 @@
+class SudokuGenerator:
+    def __init__(self, row_length=9, removed_cells):
+        self.row_length = row_length
+        self.removed_cells = removed_cells #see UI Requirements
+    #def get_board(self): #returns a 2D python list of numbers
+    def print_board(self):
+        return
+    def valid_in_row(self, row, num):
+        return
+
+
